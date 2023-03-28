0412771213fb204577df17c84f2283708454855d
aysha65 <101844883+aysha65@users.noreply.github.com>
2023-03-28 16:16:13 -0400

    schema - prod, test and auth

---
+++ server/auth.sql
@@ -0,0 +1,45 @@
+use duck_dress_prod;
+
+drop table if exists app_user_authority;
+drop table if exists app_authority;
+drop table if exists app_user;
+
+create table app_user (
+app_user_id int primary key auto_increment,
+username varchar(100) not null unique,
+password_hash varchar(100),
+authorities varchar(100),
+email varchar(260),
+hidden bit not null default(1)
+);
+
+create table app_authority (
+	app_authority_id int primary key auto_increment,
+    `name` varchar(25) not null
+);
+
+create table app_user_authority (
+	app_user_id int not null,
+    app_authority_id int not null,
+    constraint pk_app_user_authority
+		primary key (app_user_id, app_authority_id),
+	constraint fk_app_user_authority_app_user_id
+		foreign key (app_user_id)
+		references app_user(app_user_id),
+	constraint fk_app_user_authority_app_authority_id
+		foreign key (app_authority_id)
+		references app_authority(app_authority_id)
+);
+ 
+-- data
+insert into app_authority (`name`) values
+	('USER'),
+    ('ADMIN');
+    
+insert into app_user (username, password_hash) values
+	('user', '$2a$10$.zydOtTivoIcJP1CVcbEbOEaSfgDefR8yIRp3W.oZRFrvOSEOAOm6'), -- password "user"
+    ('admin', '$2a$10$8e6e6BDT1RUt0WuHIXqYb.szJZH9RYNRGScE.WlC52EB/92L6CasW'); -- password "admin"
+    
+insert into app_user_authority(app_user_id, app_authority_id) values
+	(1, 1),
+    (2, 2);
---
+++ server/duck_dress_prod.sql
@@ -0,0 +1,70 @@
+drop database if exists duck_dress_prod;
+create database duck_dress_prod;
+use duck_dress_prod;
+
+-- create tables and relationships
+create table duck (
+duck_id int primary key auto_increment,
+duck_image varchar(1000),
+hidden bit not null
+);
+ 
+create table app_user (
+app_user_id int primary key auto_increment,
+username varchar(100) not null unique,
+password_hash varchar(100),
+authorities varchar(100),
+email varchar(260),
+hidden bit not null default(1)
+);
+
+create table clothing_item (
+item_id int primary key auto_increment,
+item_type varchar(50),
+clothing_item_image varchar(1000),
+hidden bit not null
+);
+
+create table outfit (
+outfit_id int primary key auto_increment,
+duck_id int not null,
+app_user_id int not null,
+date_created date not null,
+clothing_items varchar(1000),
+posted bit not null,
+hidden bit not null,
+	constraint fk_duck_id
+		foreign key (duck_id)
+        references duck(duck_id),
+	constraint fk_app_user_id
+		foreign key (app_user_id)
+        references app_user(app_user_id)
+);
+
+create table user_outfit (
+app_user_id int not null,
+outfit_id int not null,
+	constraint pk_user_outfit
+		primary key(app_user_id, outfit_id),
+	constraint fk_user_outfit_app_user_id
+		foreign key (app_user_id)
+        references app_user(app_user_id),
+	constraint fk_user_outfit_outfit_id
+		foreign key (outfit_id)
+        references outfit(outfit_id)
+);
+
+create table comments (
+comment_id int primary key auto_increment,
+app_user_id int not null,
+outfit_id int not null,
+content varchar(500),
+date_time date not null,
+hidden bit not null,
+constraint fk_user_id
+		foreign key (app_user_id)
+        references app_user(app_user_id),
+constraint fk_outfit_id
+		foreign key (outfit_id)
+        references outfit(outfit_id)
+);
