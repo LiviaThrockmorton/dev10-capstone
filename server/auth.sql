4aeaa603e2f2d7a5433a3c9309b1eca7d26cf798
golson-greiff <122699782+golson-greiff@users.noreply.github.com>
2023-03-30 11:59:24 -0500

    java sql statements

--- server/auth.sql
+++ server/auth.sql
@@ -1,45 +1,59 @@
-use duck_dress_prod;
+use duck_dress_test;
 
-drop table if exists app_user_authority;
-drop table if exists app_authority;
+-- Change this later ^^^ 
+-- use duck_dress_prod
+
+
+drop table if exists app_user_role;
+drop table if exists app_role;
 drop table if exists app_user;
 
+-- create table app_user (
+--     app_user_id int primary key auto_increment,
+--     username varchar(50) not null unique,
+--     password_hash varchar(2048) not null,
+--     enabled bit not null default(1)
+-- );
+
 create table app_user (
-app_user_id int primary key auto_increment,
-username varchar(100) not null unique,
-password_hash varchar(100),
-authorities varchar(100),
-email varchar(260),
-hidden bit not null default(1)
+	app_user_id int primary key auto_increment,
+    username varchar(50) not null unique,
+    password_hash varchar(2048) not null,     
+-- authorities varchar(100),           
+	email varchar(260),
+	hidden bit not null default(1),
+	enabled bit not null default(1)
 );
 
-create table app_authority (
-	app_authority_id int primary key auto_increment,
-    `name` varchar(25) not null
+create table app_role (
+    app_role_id int primary key auto_increment,
+    `name` varchar(50) not null unique
 );
 
-create table app_user_authority (
-	app_user_id int not null,
-    app_authority_id int not null,
-    constraint pk_app_user_authority
-		primary key (app_user_id, app_authority_id),
-	constraint fk_app_user_authority_app_user_id
-		foreign key (app_user_id)
-		references app_user(app_user_id),
-	constraint fk_app_user_authority_app_authority_id
-		foreign key (app_authority_id)
-		references app_authority(app_authority_id)
+create table app_user_role (
+    app_user_id int not null,
+    app_role_id int not null,
+    constraint pk_app_user_role
+        primary key (app_user_id, app_role_id),
+    constraint fk_app_user_role_user_id
+        foreign key (app_user_id)
+        references app_user(app_user_id),
+    constraint fk_app_user_role_role_id
+        foreign key (app_role_id)
+        references app_role(app_role_id)
 );
 
--- data
-insert into app_authority (`name`) values
-	('USER'),
+insert into app_role (`name`) values
+    ('USER'),
     ('ADMIN');
-    
-insert into app_user (username, password_hash) values
-	('user', '$2a$10$.zydOtTivoIcJP1CVcbEbOEaSfgDefR8yIRp3W.oZRFrvOSEOAOm6'), -- password "user"
-    ('admin', '$2a$10$8e6e6BDT1RUt0WuHIXqYb.szJZH9RYNRGScE.WlC52EB/92L6CasW'); -- password "admin"
-    
-insert into app_user_authority(app_user_id, app_authority_id) values
-	(1, 1),
-    (2, 2);
+
+-- passwords are set to "P@ssw0rd!"
+insert into app_user (username, password_hash, email, hidden, enabled)
+    values
+    ('jsmith', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 'john@smith.com', 0, 1),
+    ('sjones', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 'sally@jones.com', 0, 1);
+
+insert into app_user_role
+    values
+    (1, 2),
+    (2, 1);
