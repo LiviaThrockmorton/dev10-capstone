6a24befc738daa51295463fba3b0fd7e0ed37db2
golson-greiff <122699782+golson-greiff@users.noreply.github.com>
2023-03-31 11:04:03 -0500

    data updates

--- server/duck_dress_test.sql
+++ server/duck_dress_test.sql
@@ -49,7 +49,7 @@ clothing_item_image varchar(1000),
 hidden bit not null
 );
 
-create table clothing_items (
+create table clothing_item (
 item_id int primary key auto_increment,
 shirt_id int,
 pants_id int,
@@ -70,7 +70,9 @@ outfit_id int primary key auto_increment,
 duck_id int not null,
 -- app_user_id int not null,
 date_created date not null,
-item_id int not null,
+shirt_id int not null,
+pants_id int not null,
+hat_id int not null,
 posted bit not null,
 hidden bit not null,
 	constraint fk_duck_id
@@ -79,9 +81,15 @@ hidden bit not null,
 -- 	constraint fk_app_user_id
 -- 		foreign key (app_user_id)
 --         references app_user(app_user_id),
-	constraint fk_item_id
-		foreign key (item_id)
-        references clothing_item(item_id)
+	constraint fk_shirt_id_outfit
+		foreign key (shirt_id)
+        references clothing_item(shirt_id),
+	constraint fk_pants_id_outfit
+		foreign key (pants_id)
+        references clothing_item(pants_id),
+	constraint fk_hat_id_outfit
+		foreign key (hat_id)
+        references clothing_item(hat_id)
 );
 
 create table user_outfit (
@@ -104,7 +112,7 @@ outfit_id int not null,
 content varchar(500),
 date_time date not null,
 hidden bit not null,
-constraint fk_user_id
+constraint fk_comment_app_user_id
 		foreign key (app_user_id)
         references app_user(app_user_id),
 constraint fk_outfit_id
@@ -127,19 +135,14 @@ delete from outfit;
 alter table outfit auto_increment = 1;
 
 -- inserts
-insert into outfit (outfit_id, shirt_id, pants_id, hat_id, date_created, duck_id, posted, hidden) values
-(1, 1, 1, 1, 03-28-2023, 1, true, false),
-(2, 2, 3, 5, 03-21-2023, 2, true, true),
-(3, 3, 5, 4, 02-28-2023, 3, false, false),
-(4, 5, 2, 3, 01-02-2023, 4, true, true),
-(5, 4, 4, 2, 11-28-2022, 5, false, false);
 
-insert into comments (comment_id, user_id, content, outfit_id, date_time, hidden) values
-(1, 1, 'I really liked my duck! So cute.', 1, 03-28-2023, 0),
-(2, 2, 'Are we actually going to pretend this is cute?', 2, 03-21-2023, 1),
-(3, 3, 'Here is mine.', 3, 02-28-2023, 0),
-(4, 4, 'I think I should show MY ducks now...', 4, 01-02-2023, 0),
-(5, 5, 'Very proud of this one yall!!', 5, 11-28-2022, 0);
+
+insert into comments (comment_id, app_user_id, content, outfit_id, date_time, hidden) values
+(1, 1, 'I really liked my duck! So cute.', 1, '2023-3-28', 0),
+(2, 2, 'Are we actually going to pretend this is cute?', 2, '2023-03-21', 1),
+(3, 3, 'Here is mine.', 3, '02-28-2023', 0),
+(4, 4, 'I think I should show MY ducks now...', 4, '2023-01-02', 0),
+(5, 5, 'Very proud of this one yall!!', 5, '2022-11-28', 0);
 
 insert into duck (duck_id, duck_image, hidden) values
 (1, '[INSERT URL HERE]', 0),
@@ -147,12 +150,6 @@ insert into duck (duck_id, duck_image, hidden) values
 (3, '[INSERT URL HERE]', 0),
 (4, '[INSERT URL HERE]', 1),
 (5, '[INSERT URL HERE]', 0);
-
-
-
-
-
-
 
 insert into shirts (shirt_id, item_type, clothing_item_image, hidden) values
 (3, 'Shirt', '[INSERT URL HERE]', 0),
@@ -174,6 +171,13 @@ insert into clothing_item (shirt_id, pants_id, hat_id) values
 (0, 0, 1),
 (0, 0, 2);
 
+insert into outfit (outfit_id, shirt_id, pants_id, hat_id, date_created, duck_id, posted, hidden) values
+(1, 1, 1, 1, '2023-03-28', 1, true, false),
+(2, 2, 3, 5, '2023-03-21', 2, true, true),
+(3, 3, 5, 4, '2023-02-28', 3, false, false),
+(4, 5, 2, 3, '2023-01-02', 4, true, true),
+(5, 4, 4, 2, '2023-11-28', 5, false, false);
+
 insert into user_outfit (app_user_id, outfit_id) values
 (1, 4),
 (2, 3),
@@ -183,3 +187,6 @@ insert into user_outfit (app_user_id, outfit_id) values
 
 end //
 delimiter ;
+
+
+-- Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`duck_dress_test`.`outfit`, CONSTRAINT `fk_duck_id` FOREIGN KEY (`duck_id`) REFERENCES `duck` (`duck_id`))
