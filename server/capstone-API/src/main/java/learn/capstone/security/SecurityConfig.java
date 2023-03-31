396fdbdbef93099a2ed1874327dd27c8f9dac1b5
LiviaThrockmorton <122691792+LiviaThrockmorton@users.noreply.github.com>
2023-03-31 11:17:52 -0500

    findAll ducks http request success

--- server/capstone-API/src/main/java/learn/capstone/security/SecurityConfig.java
+++ server/capstone-API/src/main/java/learn/capstone/security/SecurityConfig.java
@@ -1,28 +1,29 @@
-//package learn.capstone.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig {
-//
+package learn.capstone.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
 //    private final JwtConverter converter;
 //
 //    public SecurityConfig(JwtConverter converter) {
 //        this.converter = converter;
 //    }
-//
-//    @Bean
-//    public SecurityFilterChain getChain(HttpSecurity http, AuthenticationConfiguration config) throws Exception {
-//        http.csrf().disable();
-//        http.cors();
-//
+
+    @Bean
+    public SecurityFilterChain getChain(HttpSecurity http, AuthenticationConfiguration config) throws Exception {
+        http.csrf().disable();
+        http.cors();
+        http.authorizeRequests().antMatchers("/**").permitAll();
+
 //        http.authorizeRequests()
 //                .antMatchers(HttpMethod.POST, "/authenticate").permitAll()
 //                .antMatchers(HttpMethod.POST, "/refresh").authenticated()
@@ -35,12 +36,12 @@
 //                .addFilter(new JwtRequestFilter(manager(config), converter))
 //                .sessionManagement()
 //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public AuthenticationManager manager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-//}
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager manager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
