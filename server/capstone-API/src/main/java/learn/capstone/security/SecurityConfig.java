158cae87280aa55b88d209f92f6a850dbc756388
golson-greiff <122699782+golson-greiff@users.noreply.github.com>
2023-04-01 22:09:48 -0500

    Update SecurityConfig.java

--- server/capstone-API/src/main/java/learn/capstone/security/SecurityConfig.java
+++ server/capstone-API/src/main/java/learn/capstone/security/SecurityConfig.java
@@ -27,31 +27,33 @@ public class SecurityConfig {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/authenticate").permitAll()
                 .antMatchers(HttpMethod.POST, "/refresh").authenticated()
+
+                .antMatchers(HttpMethod.GET, "/api/item/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/duck/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/comment/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/outfit/**").permitAll()
+
+
+
                 .antMatchers(HttpMethod.POST, "/api/duck/**").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/duck/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/api/duck/**").hasAuthority("ADMIN")
 
-          
-                .antMatchers(HttpMethod.GET, "/api/item/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/item/**").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/item/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/api/item/**").hasAuthority("ADMIN")
 
-
-                .antMatchers(HttpMethod.GET, "/api/outfit/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/outfit/**").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/outfit/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/api/outfit/**").hasAuthority("ADMIN")
 
-
-                .antMatchers(HttpMethod.GET, "/api/comment/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/comment/**").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/comment/**").hasAnyAuthority("USER", "ADMIN")
+
+
+                .antMatchers(HttpMethod.DELETE, "/api/duck/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/item/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/outfit/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/api/comment/**").hasAuthority("ADMIN")
 
 
-                
                 .antMatchers("/**").denyAll()
                 .and()
                 .addFilter(new JwtRequestFilter(manager(config), converter))
