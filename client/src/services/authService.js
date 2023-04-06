4c6e8cd3c927fef6bf57acf9a45e2d6cd9c01a82
golson-greiff <122699782+golson-greiff@users.noreply.github.com>
2023-04-06 16:01:58 -0500

    making username work!

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -43,7 +43,7 @@ export async function authenticate(credentials) {
 
 //Bea's changes
 export async function findAppUser(appUserId) {
-    const response = await fetch(`${url}/user/${appUserId}`)
+    const response = await fetch(`${url}/api/user/${appUserId}`)
     if (response.ok) {
         return response.json();
     }
