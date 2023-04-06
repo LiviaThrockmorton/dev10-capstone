835fa2b9d08f51ecde50a6906916c5f4bf6a2e8b
golson-greiff <122699782+golson-greiff@users.noreply.github.com>
2023-04-06 08:59:30 -0500

    Update Comment.js

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,74 +1,29 @@
-import { Link, useNavigate } from "react-router-dom";
+function Comment({ comment }) {
 
-import { findByOutfit } from "../services/commentService";
-import Outfit from "./Outfit";
-import { useContext, useEffect, useState } from "react";
-import AuthContext from "../contexts/AuthContext";
-import { findById } from "../services/OutfitService";
-
-
-function Comment({ comment, commentUserId }) {
-
-    const [outfit, setOutfit] = useState([]);
-    const [outfitUser, setOutfitUser] = useState();
-    const [error, setError] = useState(false);
-    const { user } = useContext(AuthContext);
-
-    const navigate = useNavigate();
-
-    // const handleDelete = () => {
-    //     navigate(`/delete/${comment.commentId}`)
-    // };
-
-    // const handleEdit = () => {
-    //     navigate(`/edit/${comment.commentId}`)
-    // };
-
-
-
-    useEffect(() => {
-
-        findById(comment.outfitId)
-            .then(setOutfit)
-            .catch(() => setError(true));
-
-    }, [comment, navigate]);
-    
-
-    const username = commentUserId
-
-
-
-//format date
     function getMonthName(monthNumber) {
         const date = new Date();
         date.setMonth(monthNumber - 1);
-
         return date.toLocaleString('en-US', { month: 'long' });
     }
+
     var date = new Date(comment.dateTime);
     date = getMonthName(date.getMonth()) + ' ' + date.getDay() + ', ' + date.getFullYear();
 
-
-
-
-
     return (
-
         <div className="card-container border-light .bg-transparent " >
             <div className="card-header .bg-transparent text-white d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">{username}</h5>
-                    <small>{date}</small>
+                <h5 class="mb-1">{comment.userId}</h5>
+                <small>{date}</small>
             </div>
             <div className="card-body .bg-transparent">
-                    <p>{comment.content}</p>
+                <p>{comment.content}</p>
             </div>
             <hr class="hr" />
-            
+            <div className="d-none">{comment.commentId}</div>
+            <div className="d-none">{comment.userId}</div>
+            <div className="d-none">{comment.outfitId}</div>
+            <div className="d-none">{comment.hidden}</div>
         </div >
-
-
-
     );
 }
 
