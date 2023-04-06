b0d0b57c62d17cc17d3dbf0bf910ccc4b84d47be
aysha65 <101844883+aysha65@users.noreply.github.com>
2023-04-06 11:15:18 -0400

    toggle switch update with corbin

--- client/src/components/toggleSwitch.js
+++ client/src/components/toggleSwitch.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 
-const ToggleSwitch = ({outfitId}) => {
-  const [isChecked, setIsChecked] = useState(false);
+const ToggleSwitch = ({ outfitId, hidden, handleChange }) => {
+  const [isChecked, setIsChecked] = useState(hidden);
 
-  const handleChange = () => {
+  const onChange = () => {
+    handleChange(!isChecked);
     setIsChecked(prevState => !prevState);
   };
 
   return (
-    <label className="switch">
-      <input type="checkbox" checked={isChecked} onChange={handleChange} />
-      <span className="slider round">
-        <span className="switch-label">Posted</span>
-        {isChecked && (
-          <span className="switch-label-right">Private</span>
-        )}
-      </span>
-    </label>
+    <div>
+      <label className="switch">
+        <input type="checkbox" checked={isChecked} onChange={onChange} />
+        <span className="slider round">
+          <span className="switch-label">Posted</span>
+          {isChecked && (
+            <span className="switch-label-right">Private</span>
+          )}
+        </span>
+      </label>
+      {/* {!isChecked && (
+        <img src= {duck.duckImage} alt="Duck" />
+      )} */}
+    </div>
   );
 };
 
