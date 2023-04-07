ee31126732bb0a19f005b9d38be3ada28859d836
golson-greiff <122699782+golson-greiff@users.noreply.github.com>
2023-04-06 20:54:54 -0500

    Update BottomBar.js

--- client/src/components/BottomBar.js
+++ client/src/components/BottomBar.js
@@ -8,12 +8,18 @@ function BottomNav() {
     const auth = useContext(AuthContext);
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5 sticky-top">
-            <div className="container-fluid">
-                <div className="navbar-brand">Dress-Up Duck</div>
+        <nav className="navbar navbar-dark bg-dark ">
+            <div className="">
 
+                <div className="row">
+                    <div className="col">
+                        © What the Ducks 2023 
+                    </div>
+                    <div className="col">Terms of Use</div>
+                    <div className="col">
+                        Privacy Policy</div>
+                </div>
 
- 
             </div>
         </nav>
 
